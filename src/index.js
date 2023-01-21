a5dd55d586a74cb70463afcb9db727ebc1cc301c
Furkan Apaydin <furkanapydn@gmail.com>
2023-01-21 13:21:09 +0300

    uygulama

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+const BookList = () => {
+ return <section><Book/></section>
+}
+const Book = () => {
+ return <article>
+  <Image />
+  <Title />
+  <Author/>
+ </article>
+}
+const Image = () => <img src="https://m.media-amazon.com/images/I/41CSAHVHk+L._SY344_BO1,204,203,200_.jpg" alt=' Prince Harry The Duke of Sussex '/> 
+const Title = () => <h2>Spare Hardcover </h2>
+const Author = () => {
+ return <h2>Prince Harry </h2>
+}
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<BookList/>)
